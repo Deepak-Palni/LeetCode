b92b020820266bb0115d590f7bc31435e26f34a6
Deepak Singh Palni <deepakpalni68@gmail.com>
2026-09-19 11:19:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (100%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.c
@@ -0,0 +1,15 @@
+bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+    int closestX = xCenter;
+    int closestY = yCenter;
+    if (xCenter < x1)
+        closestX = x1;
+    else if (xCenter > x2)
+        closestX = x2;
+    if (yCenter < y1)
+        closestY = y1;
+    else if (yCenter > y2)
+        closestY = y2;
+    int dx = xCenter - closestX;
+    int dy = yCenter - closestY;
+    return dx * dx + dy * dy <= radius * radius;
+}
